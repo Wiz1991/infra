be22afae88e2d48f2f000a3210292a7762cd9f96
Wiz1991 <wizoftime92@gmail.com>
2026-03-08 12:48:39 +0100

    load from .env

--- scripts/sync_all_subs.sh
+++ scripts/sync_all_subs.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+source "$(dirname "$0")/../.env"
+
 docker run -it --rm \
   -v "${APPDATA_DIR}/bazarr-sync/config.yaml:/usr/src/app/config.yaml" \
   --network traefik \
